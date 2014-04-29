8d6a010a8a00608b151849b5804e1ba3715bacbf
Navid fotovaty <nasha260@yahoo.com>
2014-04-29 17:37:18 +0800

    saving the css as button and click on it

--- tests/test_login.py
+++ tests/test_login.py
@@ -72,8 +72,9 @@ class LoginTest(test_utilities.e2e.E2ETestBigDesktop):
     time.sleep(20)
     #browser.click_link_by_partial_href('http://dev.jatomifitness.com.my/jatomi/myPHP/buytopup.php?Amount=50&amp;PaymentId=560&amp;UserEmail=jatomitest@gmail.com&amp;UserContact=856&amp;Email=jatomitest@gmail.com')
     #browser.find_link_by_text('Proceed').click()
-    browser.find_by_css('.confirmation_proceed').click()
-    #top_up_accept_button.click()
+    accept_button = browser.find_by_css('.confirmation_proceed.form-submit')
+    accept_button.click()
+    
 
     time.sleep(15)
     self.assertEqual(browser.url, 'https://www.mobile88.com/epayment/ConfirmationPage.asp') 
